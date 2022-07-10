a156349de68bf8d8ecaff8094f1d94840f22efba
SureshKumar4238 <97436880+SureshKumar4238@users.noreply.github.com>
2022-07-10 11:12:31 +0530

    #include<stdio.h> #include<math.h> int tej(int n) {     int r,sum=0;     while(n)     {         r=n%10;         n=n/10;         sum=sum+r;     }     return sum; } int tej1(int k) { 	int c; 	c=log10(k)+1; 	return c; }
    int main()
    {
        int n,i,k;
        scanf("%d",&n);
        i=n;
        while(tej1(i)!=1)
    	{
    		k=tej(i);
    		i=k;
    	}
        printf("%d",i);
        
    }

--- Add_Digits.c
+++ Add_Digits.c
@@ -5,27 +5,29 @@ int tej(int n)
     int r,sum=0;
     while(n)
     {
-       r=n%10;
-       n=n/10;
-       sum=sum+r;
+        r=n%10;
+        n=n/10;
+        sum=sum+r;
     }
     return sum;
 }
 int tej1(int k)
 {
-    int c;
-    c=log10(k)+1;
-    return c;
+	int c;
+	c=log10(k)+1;
+	return c;
 }
+  
 int main()
 {
     int n,i,k;
     scanf("%d",&n);
     i=n;
     while(tej1(i)!=1)
-    {
-        k=tej(i);
-        i=k;
-    }
+	{
+		k=tej(i);
+		i=k;
+	}
     printf("%d",i);
+    
 }
